e288afb3a6d0393ced0466a0b1f6bb864ae794c7
Samir Mishra <smir.mishra1551@gmail.com>
2021-12-01 23:27:27 +0545

    google auth started

---
+++ server/controllers/auth/googleAuth.controller.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+
+
+passport.use(new GoogleStrategy({
+    consumerKey: GOOGLE_CONSUMER_KEY,
+    consumerSecret: GOOGLE_CONSUMER_SECRET,
+    callbackURL: "http://www.example.com/auth/google/callback"
+  },
+  function(token, tokenSecret, profile, done) {
+      User.findOrCreate({ googleId: profile.id }, function (err, user) {
+        return done(err, user);
+      });
+  }
+));
