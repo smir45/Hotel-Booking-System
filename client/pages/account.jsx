aae6186516b34a1fd8e92164c916c393ff954f35
Samir Mishra <smir.mishra1551@gmail.com>
2022-01-10 21:27:57 +0545

    account setting page added as account page

---
+++ client/pages/account.jsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+
+const AccountSetting = () => {
+  return (
+      <main>
+          <h1>Account Setting</h1>
+      </main>
+  )
+};
+
+const Account = () => {
+  return (
+    <main>
+      <AccountSetting />
+    </main>
+  );
+};
+
+
+export default Account;
