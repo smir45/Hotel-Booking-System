d389d8c8fd4a5e7816e1d7f09b0350a3c3762591
Samir Mishra <smir.mishra1551@gmail.com>
2022-01-22 20:49:20 +0545

    next updated to new version and some minor changes

---
+++ client/pages/notification.jsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import Link from 'next/link';
+import axios from 'axios';
+
+const Notification = () => {
+    return(
+        <main>
+            <head>
+                <title>Accommod | Notifications</title>
+            </head>
+        </main>
+    )
+}
+
+export default Notification;
