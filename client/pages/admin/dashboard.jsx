7b31f92ff977a624e5d58750fcdb25c1cbc2c86b
Samir Mishra <smir.mishra1551@gmail.com>
2022-02-09 23:33:45 +0545

    dashboard ui started

--- client/pages/admin/dashboard.jsx
+++ client/pages/admin/dashboard.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
-
+import HeaderStats from '../../components/componentsFiles/admin/Headers/HeaderStats'
+import AdminNavbar from '../../components/componentsFiles/admin/Navbars/AdminNavbar'
 const dashboard = () => {
   return <div>
     <h1>Dashboard</h1>
+    <HeaderStats />
+    <AdminNavbar/>
   </div>;
 };
 
