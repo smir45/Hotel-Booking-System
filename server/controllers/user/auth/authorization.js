1d887552cbb94e6b80cbb886666a5cfbb825134e
Samir Mishra <smir.mishra1551@gmail.com>
2022-02-12 23:25:08 +0545

    some changes

---
+++ server/controllers/user/auth/authorization.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization']; //Bearer TOKEN
+    const token = authHeader && authHeader.split(' ')[1];
+    if (token == null) return res.status(401).json({error:"Null token"});
+    jwt.verify(token, process.env.TOKEN_SECRET, (error, user) => {
+      if (error) return res.status(403).json({error : error.message});
+      req.user = user;
+      next();
+    });
+  }
+  
+  export {authenticateToken};
---
+++ server/utils/jwthelpers.js
@@ -0,0 +1,12 @@
+import jwt from 'jsonwebtoken';
+require('dotenv').config();
+
+//Generate an access token and a refresh token for this database user
+function jwtTokens({ user_id, user_name, user_email }) {
+  const user = { user_id, user_name, user_email}; 
+  const accessToken = jwt.sign(user, process.env.TOKEN_SECRET, { expiresIn: '20s' });
+  const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '5m' });
+  return ({ accessToken, refreshToken });
+}
+
+export {jwtTokens};
