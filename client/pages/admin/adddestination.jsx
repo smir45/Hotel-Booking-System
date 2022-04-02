530f85aa63abece2e76ef3573546b792113ad6aa
Samir Mishra <smir.mishra1551@gmail.com>
2022-04-02 22:22:18 +0545

    file upload path fixed on frontent

--- client/pages/admin/adddestination.jsx
+++ client/pages/admin/adddestination.jsx
@@ -1,25 +1,29 @@
 import React, {useState, useEffect} from "react";
-import axios from "axios";
 import Axios from "axios";
 import router from "next/router";
 import {useToasts} from "react-toast-notifications";
+
 const AddDestination = () => {
     const [error, setError] = useState(false);
-  const [data, setData] = useState({
-    title: "",
-    description: "",
-    images: "",
-    city: "",
-  });
+    const [data, setData] = useState({
+        name: "",
+        desc: "",
+        image: [],
+        city: "",
+        country: "",
+        state: "",
+    });
 
     const addDestination = (e) => {
         const url = "http://localhost:8000/api/destinations/post";
         e.preventDefault();
         Axios.post(url, {
-            title: data.title,
-            description: data.description,
-            images: data.images,
+            name: data.name,
+            desc: data.desc,
+            image: data.image,
             city: data.city,
+            country: data.country,
+            state: data.state,
         })
             .then((res) => {
                 if (res.data.message === "Destination posted successfully") {
@@ -32,7 +36,7 @@ const AddDestination = () => {
                 } else {
                     setError(true);
                 }
-                router.push('/login');
+                router.push('/admin/destinations');
             })
             .catch((err) => {
                 setError(err.response.data.message);
@@ -43,39 +47,76 @@ const AddDestination = () => {
             });
     };
     const handleChange = (e) => {
-        const newdata = { ...data };
+        const newdata = {
+            name: data.name,
+            desc: data.desc,
+            city: data.city,
+            country: data.country,
+            state: data.state,
+        };
         newdata[e.target.id] = e.target.value;
-        setData(newdata);
+        setData({
+                ...newdata,
+
+                image: e.target.files[0]
+            }
+        );
     };
-    const { addToast } = useToasts()
+    const handleImage = (e) => {
+        const NewImage = {
+            image: data.image,
+        };
+        // NewImage[e.target.id] = e.target.files[0];
+        // setData({...data, ...NewImage});
+    }
+    console.log(data);
+    const {addToast} = useToasts()
     return (
 
-           <div className="flex justify-center items-center" style={{background: "#f5f5f5", height: "100vh"}}>
-               <div className="  w-3/5 p-5 rounded-xl shadow-xl mx-auto" style={{background: "#ffffff"}}>
-                   <h1 className="text-center text-primary text-2xl font-bolder">Add Destination</h1>
-                   <form className="mx-auto" style={{ width: "fit-content"}} onSubmit={(e) => addDestination(e)}>
-                       <div>
-                           <label className="p-2">Title</label><br/>
-                           <input className="border mx-2 p-3 rounded" style={{width: "500px", background: "#f5f5f5"}} required type="text" id="title" value={data.title} onChange={(e) => handleChange(e)}/>
-                       </div>
-                       <div>
-                           <label className="p-2">Description</label><br/>
-                           <input className="border mx-2 p-3 rounded" style={{width: "500px", background: "#f5f5f5"}} required type="text" id="description" value={data.description} onChange={(e) => handleChange(e)}/>
-                       </div>
-                       <div>
-                           <label className="p-2">Image Link</label><br/>
-                           <input className="border mx-2 p-3 rounded" style={{width: "500px", background: "#f5f5f5"}} required type="text" id="images" value={data.images} onChange={(e) => handleChange(e)}/>
-                       </div>
-                       <div>
-                           <label className="p-2">City</label><br/>
-                           <input className="border mx-2 p-3 rounded" style={{width: "500px", background: "#f5f5f5"}} required type="text" id="city" value={data.city} onChange={(e) => handleChange(e)}/>
-                       </div>
+        <div className="flex justify-center items-center" style={{background: "#f5f5f5", height: "100vh"}}>
+            <div className="  w-3/5 p-5 rounded-xl shadow-xl mx-auto" style={{background: "#ffffff"}}>
+                <h1 className="text-center text-primary text-2xl font-bolder">Add Destination</h1>
+                <form className="mx-auto" style={{width: "fit-content"}} onSubmit={(e) => addDestination(e)}>
+                    <div>
+                        <label className="p-2">Title</label><br/>
+                        <input className="border mx-2 p-3 rounded" style={{width: "500px", background: "#f5f5f5"}}
+                               required type="text" id="name" value={data.name} onChange={(e) => handleChange(e)}/>
+                    </div>
+                    <div>
+                        <label className="p-2">Description</label><br/>
+                        <input className="border mx-2 p-3 rounded" style={{width: "500px", background: "#f5f5f5"}}
+                               required type="text" id="desc" value={data.desc}
+                               onChange={(e) => handleChange(e)}/>
+                    </div>
+                    <div>
+                        <label className="p-2">Image</label><br/>
+                        <input className="border mx-2 p-3 rounded" style={{width: "500px", background: "#f5f5f5"}}
+                               required type="file" id="image" onChange={(e) => handleChange(e)}/>
+                    </div>
+                    <div>
+                        <label className="p-2">City</label><br/>
+                        <input className="border mx-2 p-3 rounded" style={{width: "500px", background: "#f5f5f5"}}
+                               required type="text" id="city" value={data.city} onChange={(e) => handleChange(e)}/>
+                    </div>
+                    <div>
+                        <label className="p-2">Country</label><br/>
+                        <input className="border mx-2 p-3 rounded" style={{width: "500px", background: "#f5f5f5"}}
+                               required type="text" id="country" value={data.country}
+                               onChange={(e) => handleChange(e)}/>
+                    </div>
+                    <div>
+                        <label className="p-2">State</label><br/>
+                        <input className="border mx-2 p-3 rounded" style={{width: "500px", background: "#f5f5f5"}}
+                               required type="text" id="state" value={data.state} onChange={(e) => handleChange(e)}/>
+                    </div>
 
-                       <button className="border py-3 mb-5 rounded mx-auto mx-2 mt-4 bg-pmry hover:bg-blue-800 text-white" style={{width: "500px"}} >Submit</button>
-                   </form>
-               </div>
+                    <button className="border py-3 mb-5 rounded mx-auto mx-2 mt-4 bg-pmry hover:bg-blue-800 text-white"
+                            style={{width: "500px"}}>Submit
+                    </button>
+                </form>
+            </div>
 
-           </div>
+        </div>
     )
 };
 
--- server/controllers/hotels/upload.js
+++ server/controllers/hotels/upload.js
@@ -2,50 +2,49 @@ const imageKit = require("../../utils/imagekit.config");
 const {Destination} = require("../../models");
 
 const ImageUpload = async (req, res, next) => {
-  console.log("destinations", req.body.destinationId);
-  destinationId = req.body.destinationId;
-  if (!req.files) {
-    res.send("No file uploaded");
-    return;
-  }
-  imageKit.upload(
-    {
-      file: req.files.image.data,
-      fileName: req.files.image.name,
-      folder: "destinations",
-    },
-    async function (err, response) {
-      if (err) {
-        console.log(err);
-        return res.status(500).json({
-          status: "failed",
-          message: "An error occured during file upload. Please try again.",
-        });
-      } else {
-        const { url } = response;
-        const modifiedUrl = imageKit.url({
-          src: url,
-          transformation: [
-            {
-              height: "200",
-              width: "200",
-              quality: "150",
-              format: "png",
-              overlayText: "Accommod",
-              overlayTextColor: "white",
-              focus: "auto",
-            },
-          ],
-        });
-        res.json({
-          status: "success",
-          data: modifiedUrl,
-          message: "Successfully uploaded files",
-        });
-        // console.log("newDestinationThumbnail", newDestinationThumbnail);
-      }
+    console.log("destinations", req.body.destinationId);
+    destinationId = req.body.destinationId;
+    if (!req.files) {
+        res.send("No file uploaded");
+        return;
     }
-  );
+    imageKit.upload(
+        {
+            file: req.files.image.data,
+            fileName: req.files.image.name,
+            folder: "destinations",
+        },
+        async function (err, response) {
+            if (err) {
+                console.log(err);
+                return res.status(500).json({
+                    status: "failed",
+                    message: "An error occured during file upload. Please try again.",
+                });
+            } else {
+                const {url} = response;
+                const modifiedUrl = imageKit.url({
+                    src: url,
+                    transformation: [
+                        {
+                            height: "200",
+                            width: "200",
+                            quality: "150",
+                            format: "png",
+                            overlayText: "Accommod",
+                            overlayTextColor: "white",
+                            focus: "auto",
+                        },
+                    ],
+                });
+                res.json({
+                    status: "success",
+                    data: modifiedUrl,
+                    message: "Successfully uploaded files",
+                });
+            }
+        }
+    );
 };
 
 module.exports = ImageUpload;
