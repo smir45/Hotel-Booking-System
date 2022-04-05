494398b182ae8b4d1ccf796e73b155cf50f0e8c4
Samir Mishra <smir.mishra1551@gmail.com>
2022-04-06 01:27:47 +0545

    blog models generated also ui done

--- client/pages/blog/index.js
+++ client/pages/blog/index.js
@@ -1,62 +1,45 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import router from "next/router";
 import axios from "axios";
 
 const BlogData = () => {
-    // const [isLoading, setIsLoading] = useState(true);
-    // const [Blog, setBlog] = useState(null);
-    // const [dash, setDash] = useState(null);
-    // const datas = Blog;
-    // const daatas = dash;
-    //
-    // useEffect(() => {
-    //     const fetchData = async () => {
-    //         const result = await axios("http://localhost:8000/api/blog/post/get");
-    //         setBlog(result.data);
-    //         setIsLoading(false);
-    //         setDash(result.data.slice(0, 3));
-    //     };
-    //     fetchData();
-    // }, []);
-    // if (isLoading) {
-    //     return <div className="login-back-loading">Loading...</div>;
-    // }
-    return (
-        <>
-            {/* Section 1 */}
-            <section className="bg-white">
-                <div className="flex grid grid-cols-12 pb-10 sm:px-5 gap-x-8 gap-y-16">
-                    <div className="flex flex-col items-start col-span-12 space-y-3 sm:col-span-6 xl:col-span-4">
-                        <a href="#_" className="block">
-                            <img
-                                className="object-cover w-full mb-2 overflow-hidden rounded-lg shadow-sm max-h-56"
-                                src="https://cdn.devdojo.com/images/may2021/fruit.jpg"
-                            />
-                        </a>
-                        <div
-                            className="bg-purple-500 flex items-center px-3 py-1.5 leading-none rounded-full text-xs font-medium uppercase text-white inline-block">
-                            <span>Lifestyle</span>
-                        </div>
-                        <h2 className="text-lg font-bold sm:text-xl md:text-2xl">
-                            <a href="#_">Creating a Future Worth Living</a>
-                        </h2>
-                        <p className="text-sm text-gray-500">
-                            Learn the attributes you need to gain in order to build a future and
-                            create a life that you are truly happy with.
-                        </p>
-                        <p className="pt-2 text-xs font-medium">
-                            <a href="#_" className="mr-1 underline">
-                                Mary Jane
-                            </a>{" "}
-                            · <span className="mx-1">April 17, 2021</span> ·{" "}
-                            <span className="mx-1 text-gray-600">3 min. read</span>
-                        </p>
-                    </div>
+  return (
+    <>
+      <div className="bg-[#EEF6FC] h-[100vh]">
+        <section className="text-gray-600 body-font">
+          <div className="container px-5 py-24 mx-auto max-w-7x1">
+            <div className="flex flex-wrap w-full mb-4 p-4">
+              <div className="w-full mb-6 lg:mb-0">
+                <h1 className="sm:text-4xl text-center underline text-5xl font-medium font-bold title-font mb-2 text-gray-900">
+                  Accommod Blogs
+                </h1>
+                <div className="h-1 w-20 bg-primary rounded" />
+              </div>
+            </div>
+            <div className="flex flex-wrap -m-4">
+              <div className="xl:w-1/3 md:w-1/2 p-4">
+                <div className="bg-white p-6 border bg-[#ffff] shadow-xl rounded-xl">
+                  <img
+                    className="lg:h-60 xl:h-56 md:h-64 sm:h-72 xs:h-72 h-72  rounded w-full object-cover object-center mb-5"
+                    src="https://kuyou.id/content/images/ctc_2020021605150668915.jpg"
+                    alt="Image Size 720x400"
+                  />
+                  <h2 className="text-xl text-gray-900 font-bold text-center title-font mb-4">
+                    Chichen Itza
+                  </h2>
+                  <p className="leading-relaxed text-base">
+                    Fingerstache flexitarian street art 8-bit waistcoat.
+                    Distillery hexagon disrupt edison bulbche.
+                  </p>
                 </div>
-            </section>
-        </>
-    );
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </>
+  );
 };
 
 export default BlogData;
--- client/pages/hostels/index.jsx
+++ client/pages/hostels/index.jsx
@@ -33,63 +33,6 @@ const Hostels = () => {
                 </p>
               </div>
             </div>
-            <div className="xl:w-1/3 md:w-1/2 p-4">
-              <div className="bg-white p-6 rounded-lg">
-                <img
-                  className="lg:h-60 xl:h-56 md:h-64 sm:h-72 xs:h-72 h-72 rounded w-full object-cover object-center mb-6"
-                  src="https://asset.kompas.com/crops/Pk_pN6vllxXy1RshYsEv74Q1BYA=/56x0:1553x998/750x500/data/photo/2021/06/16/60c8f9d68ff4a.jpg"
-                  alt="Image Size 720x400"
-                />
-                <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">
-                  SUBTITLE
-                </h3>
-                <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
-                  Colosseum Roma
-                </h2>
-                <p className="leading-relaxed text-base">
-                  Fingerstache flexitarian street art 8-bit waistcoat.
-                  Distillery hexagon disrupt edison bulbche.
-                </p>
-              </div>
-            </div>
-            <div className="xl:w-1/3 md:w-1/2 p-4">
-              <div className="bg-white p-6 rounded-lg">
-                <img
-                  className="lg:h-60 xl:h-56 md:h-64 sm:h-72 xs:h-72 h-72 rounded w-full object-cover object-center mb-6"
-                  src="https://images.immediate.co.uk/production/volatile/sites/7/2019/07/33-GettyImages-154260931-216706f.jpg?quality=90&resize=768%2C574"
-                  alt="Image Size 720x400"
-                />
-                <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">
-                  SUBTITLE
-                </h3>
-                <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
-                  Great Pyramid of Giza
-                </h2>
-                <p className="leading-relaxed text-base">
-                  Fingerstache flexitarian street art 8-bit waistcoat.
-                  Distillery hexagon disrupt edison bulbche.
-                </p>
-              </div>
-            </div>
-            <div className="xl:w-1/3 md:w-1/2 p-4">
-              <div className="bg-white p-6 rounded-lg">
-                <img
-                  className="lg:h-60 xl:h-56 md:h-64 sm:h-72 xs:h-72 h-72 rounded w-full object-cover object-center mb-6"
-                  src="https://wisatamuda.com/wp-content/uploads/2019/02/1-Golden-Gate-Bridge-Gambar-dan-Foto-Tempat-Wisata-Terbaik-di-San-Fransisco-USA.jpg"
-                  alt="Image Size 720x400"
-                />
-                <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">
-                  SUBTITLE
-                </h3>
-                <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
-                  San Francisco
-                </h2>
-                <p className="leading-relaxed text-base">
-                  Fingerstache flexitarian street art 8-bit waistcoat.
-                  Distillery hexagon disrupt edison bulbche.
-                </p>
-              </div>
-            </div>
           </div>
         </div>
       </section>
